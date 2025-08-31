4e2f848b22159aa46103f1f2abf8c2551923584e
doluev <140840005+doluev@users.noreply.github.com>
2025-08-31 12:53:02 +0300

    Update server.py

--- server.py
+++ server.py
@@ -35,4 +35,4 @@ from flask import Flask, request, jsonify
        return response
 
    if __name__ == '__main__':
-       app.run(host='0.0.0.0', port=5000)
+       server.run(host='0.0.0.0', port=5000)
